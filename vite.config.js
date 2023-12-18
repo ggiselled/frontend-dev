7b7cbb38c722144395091b70742d84272cd6bfe9
Giselle <gdgoenaga@gmail.com>
2023-12-18 19:14:18 -0300

    cambios vite-config

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,6 @@ import react from '@vitejs/plugin-react-swc'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: '/frontend-dev',
+  base: '/frontend-dev/',
   plugins: [react()],
 })
