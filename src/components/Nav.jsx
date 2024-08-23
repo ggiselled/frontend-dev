a35abc7f829cfd79e7c9635b5d2ff78199c9ebdc
Giselle <gdgoenaga@gmail.com>
2024-08-22 21:45:04 -0300

    v1.2 mueve nav a la derecha

--- src/components/Nav.jsx
+++ src/components/Nav.jsx
@@ -1,7 +1,5 @@
 import '../assets/styles/nav.css';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import { faGithub } from '@fortawesome/free-brands-svg-icons';
-import { faLinkedinIn } from '@fortawesome/free-brands-svg-icons';
 import { motion } from 'framer-motion';
 import cv from '/CV-Giselle Goenaga-Frontend(esp).pdf'
 
@@ -18,25 +16,6 @@ export const Nav = () => {
                 <li><a href="#proyectos">linkedin</a></li>
                 <li><a href="#contacto">email</a></li>
             </ul>
-            <div className="nav-redes">
-                <motion.a
-                href='https://github.com/ggiselled'
-                target='_blank'
-                whileHover={{scale: 1.2}}
-                whileTap={{scale: 0.8}}
-                >
-                    <FontAwesomeIcon icon={faGithub} style={{ color: '212121'}} className='icon'/>
-                </motion.a>
-                <motion.a
-                href='https://www.linkedin.com/in/ggiselled/'
-                target='_blank'
-                whileHover={{scale: 1.2}}
-                whileTap={{scale: 0.8}}
-                >
-                    <FontAwesomeIcon icon={faLinkedinIn} style={{ color: '212121' }} className='icon'/>   
-                </motion.a>
-
-            </div>
         </nav>
         </>
     )
