42920dd105da893f9bea8fe71c6371738af23284
Giselle <gdgoenaga@gmail.com>
2024-08-24 18:30:03 -0300

    cambios

--- src/components/Hola.jsx
+++ src/components/Hola.jsx
@@ -14,8 +14,8 @@ export const Hola = () => {
             </div>
             <div className="hola-description">
                 <h1>Hola, mi nombre es Giselle.</h1>
-                <h2>Soy Desarrolladora Frontend Jr en entorno web.</h2>
-                <h2>Tengo conocimientos en React, Vue, Spline y Laravel.</h2>
+                <h2>Soy Desarrolladora Frontend web.</h2>
+                <h2>Tecnlogías utlizadas: Vue JS, React JS.</h2>
                 <h2>Actualmente me encuentro en Buenos Aires, Argentina.</h2>
                 <h2>Soy estudiante de la Tecnicatura en Programación en la Universidad Nacional de Quilmes.</h2>
                 <div className='hola-cv'>
