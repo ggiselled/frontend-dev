c28c2aa3d55ae74005369a3438c0f6ff9b76a6d3
Giselle <gdgoenaga@gmail.com>
2024-08-25 13:23:20 -0300

    cambios

--- src/components/Hola.jsx
+++ src/components/Hola.jsx
@@ -1,78 +1,59 @@
-import React, { useEffect, useState } from 'react';
-import { motion } from 'framer-motion';
-import Atom from './spline/Atom.jsx';
+import React, { useEffect, useState } from "react";
+import { motion } from "framer-motion";
+import Atom from "./spline/Atom.jsx";
 
-import '../assets/styles/hola.css';
+import "../assets/styles/hola.css";
 
 export const Hola = () => {
   const [currentTextIndex, setCurrentTextIndex] = useState(0);
-  const [scrollDelta, setScrollDelta] = useState(0); // Acumulador del delta
+  const [scrollDelta, setScrollDelta] = useState(0);
 
-
-  //Manejo del scroll de texto
   useEffect(() => {
     const handleWheel = (event) => {
-      const newDelta = event.deltaY
+      const newDelta = event.deltaY;
       setScrollDelta((prevDelta) => prevDelta + newDelta);
 
       if (newDelta > 0 && scrollDelta > 100 && currentTextIndex < 4) {
         setCurrentTextIndex((prevIndex) => prevIndex + 1);
-        setScrollDelta(0); // Reiniciar el acumulador después de avanzar
+        setScrollDelta(0);
       } else if (newDelta < 0 && scrollDelta < -100 && currentTextIndex > 0) {
         setCurrentTextIndex((prevIndex) => prevIndex - 1);
-        setScrollDelta(0); // Reiniciar el acumulador después de retroceder
+        setScrollDelta(0);
       }
 
-      event.preventDefault(); // Prevenir el desplazamiento de la página
+      event.preventDefault();
     };
 
-    window.addEventListener('wheel', handleWheel, { passive: false });
+    window.addEventListener("wheel", handleWheel, { passive: false });
     return () => {
-      window.removeEventListener('wheel', handleWheel);
+      window.removeEventListener("wheel", handleWheel);
     };
   }, [scrollDelta, currentTextIndex]);
-  
 
   return (
     <>
       <Atom />
       <main className="hola-container">
         <div className="hola-description">
-          <motion.div
-            initial={{ opacity: 0, y: 50 }}
-            animate={{ opacity: currentTextIndex === 0 ? 1 : 0, y: currentTextIndex === 0 ? 0 : 50 }}
-            transition={{ duration: 0.5 }}
-          >
-            <h1>hi, I'm Giselle</h1>
-          </motion.div>
-          <motion.div
-            initial={{ opacity: 0, y: 50 }}
-            animate={{ opacity: currentTextIndex === 1 ? 1 : 0, y: currentTextIndex === 1 ? 0 : 50 }}
-            transition={{ duration: 0.5 }}
-          >
-            <h1>a frontend web developer</h1>
-          </motion.div>
-          <motion.div
-            initial={{ opacity: 0, y: 50 }}
-            animate={{ opacity: currentTextIndex === 2 ? 1 : 0, y: currentTextIndex === 2 ? 0 : 50 }}
-            transition={{ duration: 0.5 }}
-          >
-            <h1>committed to creating immersive web experiences</h1>
-          </motion.div>
-          <motion.div
-            initial={{ opacity: 0, y: 50 }}
-            animate={{ opacity: currentTextIndex === 3 ? 1 : 0, y: currentTextIndex === 3 ? 0 : 50 }}
-            transition={{ duration: 0.5 }}
-          >
-            <h1>based in Buenos Aires, Argentina</h1>
-          </motion.div>
-          <motion.div
-            initial={{ opacity: 0, y: 50 }}
-            animate={{ opacity: currentTextIndex === 4 ? 1 : 0, y: currentTextIndex === 4 ? 0 : 50 }}
-            transition={{ duration: 0.5 }}
-          >
-            <h1>available for remote work</h1>
-          </motion.div>
+          {[
+            "hi, I'm Giselle",
+            "a frontend web developer",
+            "committed to creating immersive web experiences",
+            "based in Buenos Aires, Argentina",
+            "available for remote work",
+          ].map((text, index) => (
+            <motion.div
+              key={index}
+              initial={{ opacity: 0, y: 50 }}
+              animate={{
+                opacity: currentTextIndex === index ? 1 : 0,
+                y: currentTextIndex === index ? 0 : 50,
+              }}
+              transition={{ duration: 0.5 }}
+            >
+              <h1>{text}</h1>
+            </motion.div>
+          ))}
         </div>
       </main>
     </>
--- src/components/Proyectos.jsx
+++ src/components/Proyectos.jsx
@@ -1,122 +1,118 @@
 import { motion, useTransform, useScroll } from "framer-motion";
 import { useRef, useState, useEffect } from "react";
-import '../assets/styles/proyectos.css';
+import "../assets/styles/proyectos.css";
 
-import bubba1 from '../assets/img/bubba1c2.jpg'
-import bubba2 from '../assets/img/bubba2c2.jpg'
-import bubba3 from '../assets/img/bubba3c2.jpg'
-import ce1 from '../assets/img/ce2.jpg'
-import ce2 from '../assets/img/cece2.jpg'
-import ev from '../assets/img/evoltis2.jpg'
-import ong from '../assets/img/hdc1c2.jpg'
-import sbux1 from '../assets/img/sbux1c2.jpg'
-import sbux2 from '../assets/img/sbux2c2.jpg'
-import sbuxx1 from '../assets/img/sbuxx1c2.jpg'
-import sbuxx2 from '../assets/img/sbuxx2c2.jpg'
-
-
-
-
-
+import bubba1 from "../assets/img/bubba1c2.jpg";
+import bubba2 from "../assets/img/bubba2c2.jpg";
+import bubba3 from "../assets/img/bubba3c2.jpg";
+import ce1 from "../assets/img/ce2.jpg";
+import ce2 from "../assets/img/cece2.jpg";
+import ev from "../assets/img/evoltis2.jpg";
+import ong from "../assets/img/hdc1c2.jpg";
+import sbux1 from "../assets/img/sbux1c2.jpg";
+import sbux2 from "../assets/img/sbux2c2.jpg";
+import sbuxx1 from "../assets/img/sbuxx1c2.jpg";
+import sbuxx2 from "../assets/img/sbuxx2c2.jpg";
 
 // Tus tarjetas originales
 //Las descriptions mantienen el centrado del div según la cantidad de palabras que tengan
 const originalCards = [
   {
     url: bubba1,
-    title: 'Bubba Bags',
+    title: "Bubba Bags",
     id: 1,
-    description: "Listado dinámico colecciones de productos | Conexión store Vuex para obtener los datos de las colecciones | Evento que selecciona la colección y redirige al usuario a la página del producto seleccionado"
+    description:
+      "Listado dinámico colecciones de productos | Conexión store Vuex para obtener los datos de las colecciones | Evento que selecciona la colección y redirige al usuario a la página del producto seleccionado",
   },
   {
     url: bubba2,
-    title: 'Bubba Bags',
+    title: "Bubba Bags",
     id: 2,
-    description: "Selección de un producto de la colección | Selección de colores disponibles | Visualización del producto en 3D | Conexión al store Vuex para obtener datos sobre la colección y productos"
-
+    description:
+      "Selección de un producto de la colección | Selección de colores disponibles | Visualización del producto en 3D | Conexión al store Vuex para obtener datos sobre la colección y productos",
   },
   {
     url: bubba3,
-    title: 'Bubba Bags',
+    title: "Bubba Bags",
     id: 3,
-    description: "Formulario interactivo | Obtención de datos a través de Firebase "
+    description:
+      "Formulario interactivo | Obtención de datos a través de Firebase ",
   },
   {
     url: ce1,
-    title: 'CIRCULAR ECONOMY',
+    title: "CIRCULAR ECONOMY",
     id: 4,
-    description: "Creado con Vue + Inertia + Laravel | Manejo de formularios de inicio de sesión y registro | Conexión de formularios con Laravel | Autenticación de usuarios | Manejo entre páginas con Inertia"
+    description:
+      "Creado con Vue + Inertia + Laravel | Manejo de formularios de inicio de sesión y registro | Conexión de formularios con Laravel | Autenticación de usuarios | Manejo entre páginas con Inertia",
   },
   {
     url: ce2,
-    title: 'CIRCULAR ECONOMY',
+    title: "CIRCULAR ECONOMY",
     id: 5,
-    description: "Acceso a diferentes secciones del evento en línea | Componente streaming principal | Acceso a otras salas de streaming"
+    description:
+      "Acceso a diferentes secciones del evento en línea | Componente streaming principal | Acceso a otras salas de streaming",
   },
   {
     url: sbux1,
-    title: 'STARBUCKS',
+    title: "STARBUCKS",
     id: 6,
-    description: "Experiencia interactiva | Rutas manejadas con vue-router"
+    description: "Experiencia interactiva | Rutas manejadas con vue-router",
   },
   {
     url: sbux2,
-    title: 'STARBUCKS',
+    title: "STARBUCKS",
     id: 7,
-    description: "Inmersión 3D a través de la implementación de Kunstmatrix"
-
+    description: "Inmersión 3D a través de la implementación de Kunstmatrix",
   },
   {
     url: ev,
-    title: 'CX CONGRESS',
+    title: "CX CONGRESS",
     id: 8,
-    description: "Experiencia interactiva | Inmersión 3D a través de la implementación del software Spline"
+    description:
+      "Experiencia interactiva | Inmersión 3D a través de la implementación del software Spline",
   },
   {
     url: sbuxx1,
-    title: 'STARBUCKS',
+    title: "STARBUCKS",
     id: 9,
-    description: "Formulario de inicio de sesión y registro conectado con Laravel"
-
+    description:
+      "Formulario de inicio de sesión y registro conectado con Laravel",
   },
   {
     url: sbuxx2,
-    title: 'STARBUCKS',
+    title: "STARBUCKS",
     id: 10,
-    description: "Inmersión 3D a través de la implementación del software Spline"
-
+    description:
+      "Inmersión 3D a través de la implementación del software Spline",
   },
   {
     url: ong,
-    title: 'ONG',
+    title: "ONG",
     id: 11,
-    description: "Desarrollo en React | Animaciones con Framer Motion"
-
+    description: "Desarrollo en React | Animaciones con Framer Motion",
   },
-
-
 ];
-
 
 export const Proyectos = () => {
   const targetRef = useRef(null);
   const { scrollYProgress } = useScroll({ target: targetRef });
   const cardWidth = 462;
   const gap = 4;
-  const totalWidth = originalCards.length * (cardWidth + gap) - window.innerWidth;
+  const totalWidth =
+    originalCards.length * (cardWidth + gap) - window.innerWidth;
   const x = useTransform(scrollYProgress, [0, 1], ["0%", `-${totalWidth}px`]);
 
   const [hoveredCard, setHoveredCard] = useState(null);
-  const [activeDescription, setActiveDescription] = useState('');
+  const [activeDescription, setActiveDescription] = useState("");
 
   // Calcula el extremo del scroll en porcentaje
-  const scrollEnd = totalWidth / (originalCards.length * cardWidth) * 100;
+  const scrollEnd = (totalWidth / (originalCards.length * cardWidth)) * 100;
 
   useEffect(() => {
     const checkScrollEdges = () => {
       const currentScroll = scrollYProgress.get() * 100;
       if (currentScroll <= 0 || currentScroll >= scrollEnd) {
-        setActiveDescription('');
+        setActiveDescription("");
       }
     };
 
@@ -124,41 +120,40 @@ export const Proyectos = () => {
     return () => unsubscribe();
   }, [scrollYProgress, scrollEnd]);
 
-
   return (
     <div className="prueba">
-
-    <section id='proyectos' ref={targetRef} className="proyectos-container">
-      <div className="carousel">
-        <motion.div style={{ x }} className="cards-container">
-          {originalCards.map((card, index) => (
-            <Card
-              key={index}
-              card={card}
-              setIsHovered={() => {
-                setHoveredCard(card.id);
-                setActiveDescription(card.description);
-              }}
-              clearHovered={() => {
-                setHoveredCard(null);
-                setActiveDescription('');
-              }}
-            />
-          ))}
-        </motion.div>
-      </div>
-      <Description
-        description={activeDescription}
-        isHovered={hoveredCard !== null}
-      />
-    </section>
+      <section id="proyectos" ref={targetRef} className="proyectos-container">
+        <div className="carousel">
+          <motion.div style={{ x }} className="cards-container">
+            {originalCards.map((card, index) => (
+              <Card
+                key={index}
+                card={card}
+                setIsHovered={() => {
+                  setHoveredCard(card.id);
+                  setActiveDescription(card.description);
+                }}
+                clearHovered={() => {
+                  setHoveredCard(null);
+                  setActiveDescription("");
+                }}
+              />
+            ))}
+          </motion.div>
+        </div>
+        <Description
+          description={activeDescription}
+          isHovered={hoveredCard !== null}
+        />
+      </section>
     </div>
   );
 };
 
 const Card = ({ card, setIsHovered, clearHovered }) => {
   return (
-    <div className="card"
+    <div
+      className="card"
       onMouseEnter={setIsHovered}
       onMouseLeave={clearHovered}
     >
@@ -184,18 +179,4 @@ const Description = ({ description, isHovered }) => {
       <p>{description}</p>
     </motion.div>
   );
-  }
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+};
